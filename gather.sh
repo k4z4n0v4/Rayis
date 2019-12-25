33393000f6879ef7632b2659eb59c00b8bdf5a10
Suren Manukyan <lindeman666666@gmail.com>
2019-12-25 23:13:41 +0400

    cleanup nvim, start gather.sh

---
+++ gather.sh
@@ -0,0 +1,5 @@
+#!/bin/env sh 
+
+echo "\n\nConfiguring vim-plug...\n\n"
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \ 
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
