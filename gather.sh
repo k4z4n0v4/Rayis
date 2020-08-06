bd3752fce20b282617bd266884da1c0ff27ecd75
Suren Manukyan <lindeman666666@gmail.com>
2020-08-06 09:27:58 +0400

    Polished gather.sh

--- gather.sh
+++ gather.sh
@@ -1,14 +1,19 @@
 #!/bin/env sh 
 
+
+
+
 echo "[#] Installing Packages..."
-sudo pacman -S awk tr 
+sudo pacman -Syu awk coreutils 
+
+sudo awk -i inplace -v beg='#[multilib]' -v num=2 '$0==beg{c=num} c&&c--{sub(/#/,"")} 1' /etc/pacman.conf
 
 packages=$(awk '/#####/ {exit} {print $1}' installed.txt | tr '\n' ' ')
 aur=$(awk '{if(found) print $1} /#####/{found=1}' installed.txt | tr '\n' ' ')
 
-pacman -S $packages
+sudo pacman -Sy $packages
 
-sudo -E aura-bin -Akax $aur
+sudo -E aura/aura-bin -Akax $aur
 
 cd dmenu/
 make && sudo make install
@@ -21,11 +26,11 @@ echo "[+] Packages successfully installed!"
 echo "[#] Configuring home..."
 
 cd ..
+mkdir -p $HOME/.config/
 cp -r .config/*  $HOME/.config/ 
 cp  .bashrc .bash_profile .Xresources $HOME/
 
 echo "[#] Home done!"
 
 echo "[#] Installing vim-plug..."
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \ 
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
